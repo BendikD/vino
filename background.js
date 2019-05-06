9c10b44cf846bdb5d36dbcebae82a802e1ec5775
Deraas <Bendik.Deraas@gmail.com>
2019-05-06 15:26:00 +0200

    XML block workaround
    - Inserted XML check up in backgroundscript.
    - Implemented communications between background and contentscript.

--- background.js
+++ background.js
@@ -10,3 +10,103 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
         chrome.pageAction.show(sender.tab.id);
     }
 });
+
+
+chrome.runtime.onMessage.addListener(function(message) {
+    if (message.type === 'prodNum') {
+        checkScore(message.vareNr);
+    }
+    console.log('prodNum recieved ' + message.vareNr);
+});
+
+
+//Creates a url to different databases based on the prodnum, and runs it in the load function
+function checkScore(varenummer){
+   aperitifUrl = 'https://www.aperitif.no/pollisten?minPrice=&maxPrice=&q='+varenummer+'&sortBy=editorial_rating&offset=0'
+   load(aperitifUrl , getScoreApertif);
+
+   dnUrl = 'https://www.dn.no/smak/vinsok/?q='+varenummer+'&fromDate=&toDate=&useFromToDate=false&sort=reld&dateField=&facets=%7Bcm%7Dwine_available_cust%3Awaf%7Cavailable&_facets=on'
+   load(dnUrl, getScoreDN);
+
+  };
+
+//extracts the score from the apertif search site.
+function getScoreApertif( site , url){
+  scorePos = site.search('rating-points') + 12;
+  score = site[scorePos+3] + site[scorePos+4];
+  if(checkValue(score[0])){
+    //insertScore('ap', score , url , 'https://static.gfx.no/images/main/aperitif.png')
+    sendScore('ap', url, score)
+  }
+  else {
+    noScoreFound();
+    sendscore('ap','','no score found');
+  }
+
+}
+
+//Used as a function to extract the score from the SMAK database
+function getScoreDN( site , url){
+  scorePos = site.search('Poeng:') + 13;
+  score = site[scorePos + 2] + site[scorePos + 3];
+  if(checkValue(score[0])){
+    //dnScore = score;
+
+    //insertScore('dn', score, url, 'https://www.dn.no/skins/dn/gfx/SmakLogo.png')
+
+    sendScore('dn', url, score);
+  }
+  else {
+
+    noScoreFound();
+    sendScore('dn','', 'no score found');
+  }
+
+}
+
+//load function that gets string format of the input url, and runs string in callbackfunction
+function load(url, callback) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      callback(xhr.response, url);
+    }
+  }
+  xhr.open('GET', url, true);
+  xhr.send('');
+}
+
+//checks if the score is valid number.
+function checkValue(value){
+  var values = ['0','1','2','3','4','5','6','7','8','9']
+  if(values.includes(value)){
+    return true;
+  }
+  else{
+  return false;
+  }
+}
+
+function sendScore(db, url, score){
+  let message = {
+    type: 'score',
+    db: db,
+    url: url,
+    score: score
+  }
+
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, message);
+});
+
+console.log('sending score to content: ' + score);
+
+
+
+}
+
+function noScoreFound(){
+  console.log('No score');
+}
--- content.js
+++ content.js
@@ -2,8 +2,8 @@ console.log("Extention running")
 
 chrome.runtime.sendMessage({type:'showPageAction'});
 
-checkScore(getVarenummer());
 
+sendVarenummer(getVarenummer());
 
 //fetches the productnumber from url, which is the last section of the url on the vinmonopolet site.
 function getVarenummer(){
@@ -16,27 +16,42 @@ function getVarenummer(){
 function sendVarenummer( varenummer ){
   let message = {
         type: 'prodNum',
-        text: varenummer
+        vareNr: varenummer
     }
     chrome.runtime.sendMessage(message);
+    console.log('ProdNr sent: ' + varenummer);
 }
 
-//inits
-//sendVarenummer(getVarenummer());
 
 
-function insertScore(id, score, url, bilde){
+chrome.runtime.onMessage.addListener(function(message) {
+    if(message.type === 'score') {
+        insertScore(message.db, message.score, message.url);
+    }
+    console.log('Score recieved ' + message.score);
+});
+
+
+function insertScore(db, score, url){
+  if(db == 'dn'){
+    bilde = 'https://www.dn.no/skins/dn/gfx/SmakLogo.png';
+  }
+  else {
+    bilde = 'https://static.gfx.no/images/main/aperitif.png';
+  }
+
+
 
   var cList = document.getElementsByClassName('product__image-container').item(0);
 
   var e = document.createElement('ul');
-  e.setAttribute('id', id);
+  e.setAttribute('id', db);
   e.setAttribute('style', 'text-align:center;')
 
 
   //create link congfig
   var a = document.createElement('a');
-  a.setAttribute('id', id);
+  a.setAttribute('id', db);
   a.setAttribute('href', url);
   a.setAttribute('target', '_blank');
 
@@ -56,81 +71,5 @@ function insertScore(id, score, url, bilde){
   e.appendChild(d);
 
   cList.appendChild(e);
-
-}
-
-
-//Creates a url to different databases based on the prodnum, and runs it in the load function
-function checkScore(varenummer){
-   aperitifUrl = 'https://www.aperitif.no/pollisten?minPrice=&maxPrice=&q='+varenummer+'&sortBy=editorial_rating&offset=0'
-   load(aperitifUrl , getScoreApertif);
-
-   dnUrl = 'https://www.dn.no/smak/vinsok/?q='+varenummer+'&fromDate=&toDate=&useFromToDate=false&sort=reld&dateField=&facets=%7Bcm%7Dwine_available_cust%3Awaf%7Cavailable&_facets=on'
-   load(dnUrl, getScoreDN);
-
-  };
-
-//extracts the score from the apertif search site.
-function getScoreApertif( site , url){
-  scorePos = site.search('rating-points') + 12;
-  score = site[scorePos+3] + site[scorePos+4];
-  if(checkValue(score[0])){
-    insertScore('ap', score , url , 'https://static.gfx.no/images/main/aperitif.png')
-  }
-  else {
-    noScoreFound();
-  }
-
-}
-
-//Used as a function to extract the score from the SMAK database
-function getScoreDN( site , url){
-  scorePos = site.search('Poeng:') + 13;
-  score = site[scorePos + 2] + site[scorePos + 3];
-  if(checkValue(score[0])){
-    //dnScore = score;
-
-    insertScore('dn', score, url, 'https://www.dn.no/skins/dn/gfx/SmakLogo.png')
-  }
-  else {
-    dnUrl='';
-    noScoreFound();
-  }
-
-}
-
-//load function that gets string format of the input url, and runs string in callbackfunction
-function load(url, callback) {
-  var xhr = new XMLHttpRequest();
-
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-      callback(xhr.response, url);
-    }
-  }
-  xhr.open('GET', url, true);
-  xhr.send('');
-}
-
-//checks if the score is valid number.
-function checkValue(value){
-  var values = ['0','1','2','3','4','5','6','7','8','9']
-  if(values.includes(value)){
-    return true;
-  }
-  else{
-  return false;
-  }
-}
-
-function noScoreFound(){
-  console.log('No score');
-  //var p = document.createElement('var')
-
-  //p.innerHTML = 'Mangler score'
-
-  //append p in score list
- //  main = document.getElementById('');
-  //main.appendChild(p);
 
 }
